b4d786ccc261c68152df4a8ca302f99c11191e68
Dan Hutchinson <danhutch5280@gmail.com>
2019-01-19 19:19:51 -0700

    renamed test

--- quickSort-test.js
+++ quickSort-test.js
@@ -11,7 +11,7 @@ describe("quickSort", function() {
   it("can sort an array with duplicate values, negative numbers, and zeros", function() {
     assert.deepEqual(quickSort([4, 0, 0, -2, -1, -2, 3, 5, 5, 2, 1, 2]), [-2, -2, -1, 0, 0, 1, 2, 2, 3, 4, 5, 5]);
   })
-  it("can sort an array with duplicate values, negative numbers, and zeros", function() {
+  it("can even sort a string", function() {
     assert.deepEqual(quickSort("a string is not an array"), [" ", " ", " ", " ", " ", "a", "a", "a", "a", "g", "i", "i", "n", "n", "n", "o", "r", "r", "r", "s", "s", "t", "t", "y"]);
   })
 });
